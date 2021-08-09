015948d80dc593527df4665c29bf3ee02892d0e2
emerson.goulart <emerson.goulart@lifemed.com.br>
2021-08-09 18:15:03 -0300

    Implementacao de ListBox

--- cadastro.py
+++ cadastro.py
@@ -14,8 +14,9 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.SetPosition(wx.Point(posx, posy))
           self.SetBackgroundColour(color)
           self.Show(True)
-          
+
           self.mypanel = wx.Panel(self, -1)
+          self.dispositivos = ['TCH18023190', 'TCH18023191', 'TCH18023192', 'TCH18023193', 'TCH18023194', 'TCH18023195', 'TCH18023187', 'TCH18023188']
 
           #Nome  
           self.lbl_nome = wx.StaticText(self, -1, "Nome  ", (10, 12))
@@ -23,7 +24,7 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_nome.SetFont(self.font)          
 
-          self.nome = wx.TextCtrl(self, pos = (10, 28), size = (405,25))
+          self.txt_nome = wx.TextCtrl(self, pos = (10, 28), size = (405,25))
 
           #Prontuario
           self.lbl_prontuario = wx.StaticText(self, -1, "Prontuario  ", (10, 55))
@@ -31,7 +32,7 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_prontuario.SetFont(self.font)
           
-          self.prontuario = wx.TextCtrl(self, pos = (10, 70), size = (200,25))          
+          self.txt_prontuario = wx.TextCtrl(self, pos = (10, 70), size = (200,25))          
           
           #Data entrada
           self.lbl_data_entrada = wx.StaticText(self, -1, "Data Entrada ", (215, 55))
@@ -39,7 +40,7 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_data_entrada.SetFont(self.font)
 
-          self.data_entrada = wx.TextCtrl(self, pos = (215, 70), size = (200,25))
+          self.txt_data_entrada = wx.TextCtrl(self, pos = (215, 70), size = (200,25))
 
           #CPF
           self.lbl_cpf = wx.StaticText(self, -1, "CPF ", (10, 98))
@@ -47,7 +48,7 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_cpf.SetFont(self.font)
 
-          self.cpf = wx.TextCtrl(self, pos = (10, 115), size = (200,25))
+          self.txt_cpf = wx.TextCtrl(self, pos = (10, 115), size = (200,25))
 
           #Telefone
           self.lbl_telefone = wx.StaticText(self, -1, "Telefone ", (215, 98))
@@ -55,7 +56,7 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_telefone.SetFont(self.font)
 
-          self.telefone = wx.TextCtrl(self, pos = (215, 115), size = (200,25))
+          self.txt_telefone = wx.TextCtrl(self, pos = (215, 115), size = (200,25))
 
           #Genero
           self.lbl_genero = wx.StaticText(self, -1, "Genero ", (10, 140))
@@ -63,7 +64,7 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_genero.SetFont(self.font)
 
-          self.genero = wx.TextCtrl(self, pos = (10, 155), size = (200,25))
+          self.txt_genero = wx.TextCtrl(self, pos = (10, 155), size = (200,25))
 
           #Data Nascimento
           self.lbl_birth_day = wx.StaticText(self, -1, "Data Nascimento ", (215, 140))
@@ -71,7 +72,7 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_birth_day.SetFont(self.font)
 
-          self.birth_day = wx.TextCtrl(self, pos = (215, 155), size = (200,25))
+          self.txt_birth_day = wx.TextCtrl(self, pos = (215, 155), size = (200,25))
 
           #Medico responsavel
           self.lbl_medico = wx.StaticText(self, -1, "Medico responsavel ", (10, 184))
@@ -79,11 +80,11 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_medico.SetFont(self.font)
 
-          self.medico = wx.TextCtrl(self, pos = (10, 200), size = (400,25))
+          self.txt_medico = wx.TextCtrl(self, pos = (10, 200), size = (400,25))
 
           #buttons
-          self.cadastrar = wx.Button(self, -1, 'Cadastrar(+)', pos = (10, 235), size = (180, 25))
-          self.alta = wx.Button(self, -1, 'Dar Alta(-)', pos = (228, 235), size = (180, 25))
+          self.btn_cadastrar = wx.Button(self, -1, 'Cadastrar(+)', pos = (10, 235), size = (180, 25))
+          self.btn_alta = wx.Button(self, -1, 'Dar Alta(-)', pos = (228, 235), size = (180, 25))
           
           #
           self.lbl_disp_plus = wx.StaticText(self, -1, "Dispositivos Disponiveis  ", (428, 12))
@@ -91,20 +92,51 @@ class Cadastro(wx.Frame):	#classe herdada da classe "Frame"
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
           self.lbl_disp_plus.SetFont(self.font)          
 
-          self.dispositivo_p = wx.TextCtrl(self, pos = (428, 28), size = (180,195), style = wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_AUTO_URL)          
-          
-          self.dispositivo_plus = wx.Button(self, -1, '->', pos = (615, 28), size = (50, 25))
+          self.lst_disp_disp = wx.ListBox(self, choices = self.dispositivos, pos = (428, 28), size = (180,195), style = wx.LB_SINGLE)
+          #self.txt_disp_disp = wx.TextCtrl(self, pos = (428, 28), size = (180,195), style = wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_AUTO_URL)          
+          self.btn_disp_right = wx.Button(self, -1, '->', pos = (615, 28), size = (50, 25))
 
           #  
           self.lbl_disp_plus = wx.StaticText(self, -1, "Dispositivos Alocados  ", (672, 12))
           self.lbl_disp_plus.SetForegroundColour('WHITE')
           self.font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
-          self.lbl_disp_plus.SetFont(self.font)          
+          self.lbl_disp_plus.SetFont(self.font)
 
-          self.dispositivo_m = wx.TextCtrl(self, pos = (672, 28), size = (180,195), style = wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_AUTO_URL)          
-          self.dispositivo_minus = wx.Button(self, -1, '<-', pos = (615, 60), size = (50, 25))
+          #self.txt_dispositivo_alloc = wx.TextCtrl(self, pos = (672, 28), size = (180,195), style = wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_AUTO_URL)
+          self.lst_disp_aloc = wx.ListBox(self, pos = (672, 28), size = (180,195), style = wx.LB_SINGLE)
+          self.btn_disp_left = wx.Button(self, -1, '<-', pos = (615, 60), size = (50, 25))
 
-          
+          #events
+          self.btn_cadastrar.Bind(wx.EVT_BUTTON, self.CadastrarOnClicked)
+          self.btn_alta.Bind(wx.EVT_BUTTON, self.AltaOnClicked)
+          self.btn_disp_right.Bind(wx.EVT_BUTTON, self.RightOnClicked)
+          self.btn_disp_left.Bind(wx.EVT_BUTTON, self.LeftOnClicked)
+
+          #self.txt_disp_disp.Bind(EVT_TEXT, self.OnRowSelected)
+          self.lst_disp_disp.Bind(wx.EVT_LISTBOX, self.OnDispDispRowSelected, self.lst_disp_disp)
+          self.lst_disp_aloc.Bind(wx.EVT_LISTBOX, self.OnDispAlocRowSelected, self.lst_disp_aloc)
+
+	def CadastrarOnClicked(self, event):
+          print("click button cadastrar")
+          #self.txt_disp_disp.AppendText("TCH1921681105\n")
+          #self.lst_disp_disp.Append("TCH1921681105")
+
+	def AltaOnClicked(self, event):
+          print("click button alta", self.txt_disp_disp.GetLineText(1))          
+
+	def RightOnClicked(self, event):
+          print("click button Right", self.txt_disp_disp.PositionToXY(12))          
+
+	def LeftOnClicked(self, event):
+          print("click button Left")
+
+	def OnDispDispRowSelected(self, event):
+          #print("Row selected ", self.lst_disp_disp.GetSelection)
+          print("Current selection: ", self.lst_disp_disp.GetStringSelection()+"\n")
+
+	def OnDispAlocRowSelected(self, event):
+          print("Disp Alocados Row selected")          
+
 
 # Codigo de inicializacao
 print "Inicializando modulo cadastro..."
